524e07a62db14545eef471cdee0c9f3fec109475
vaishnavaohio <76029991+vaishnavaohio@users.noreply.github.com>
2021-01-09 02:14:53 -0500

    Task 1.3
    If applied, this commit will have edits that were made to complete task 1.3 on careerfoundry.

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,26 @@
 let pokemonList = [
 {
     name: "pikachu",
-    types: ["grass"],
+    types: ["grass", "flying"],
     height: 3.50,
 },
 {
     name: "charzard",
-    types: ["poision"],
+    types: ["poision", "fire"],
     height: 3.25,
 },
 {
     name: "slacking",
-    types: ["fire"],
+    types: ["fire", "air"],
     height: 4.5,
 }
 ];
-    
+
+//for loop looping through objects in pokemonList array
+for (let i=0; i<pokemonList.length; i++) {
+    document.write(`<p>${pokemonList[i].name}</p>`)
+    if(pokemonList[i].height>3.30){
+        document.write("Wow, that’s big!")
+    }
+}
+
