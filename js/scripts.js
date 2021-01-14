53782d37a576122f0ddfa4ffbae708869e3d6470
vaishnavaohio <76029991+vaishnavaohio@users.noreply.github.com>
2021-01-14 15:28:38 -0500

    Edits for javascript functions part 2
    If applied, this commit will have edits that were made to complete task 1.5 on careerfoundry.

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,37 @@
+let pokemonRepository = function(){
+    let pokemonListValue = [
+    {
+        name: "pikachu",
+        types: ["grass", "flying"],
+        height: 3.50,
+    },
+    {
+        name: "charzard",
+        types: ["poision", "fire"],
+        height: 3.25,
+    },
+    {
+        name: "slacking",
+        types: ["fire", "air"],
+        height: 4.5,
+    }]  
+    return{
+        getAll:function(){
+            return pokemonListValue
+        },
+        Add: function(item){
+            pokemonListValue.push(item)
+        }
+    }
 
-let pokemonList = [
-{
-    name: "pikachu",
-    types: ["grass", "flying"],
-    height: 3.50,
-},
-{
-    name: "charzard",
-    types: ["poision", "fire"],
-    height: 3.25,
-},
-{
-    name: "slacking",
-    types: ["fire", "air"],
-    height: 4.5,
-}
-];
-
+}();
+pokemonRepository.Add({ name: 'pikachu' });
 //for loop looping through objects in pokemonList array
-for (let i=0; i<pokemonList.length; i++) {
-    document.write(`<p>${pokemonList[i].name}</p>`)
-    if(pokemonList[i].height>3.30){
+pokemonRepository.getAll().forEach(function(pokemonLoop){
+    document.write(`<p>${pokemonLoop.name}</p>`)
+
+    if(pokemonLoop.height>3.30){
         document.write("Wow, that’s big!")
     }
-}
-
+   // pokemonList.foreach(pokemonLoop)
+})
